ad00c9b53dc921da1371e5523227c791651ccf88
Oskar Torgersrud <oskartorgersrud@hotmail.com>
2025-03-19 18:01:16 +0100

    initial demo test

--- main.py
+++ main.py
@@ -1,12 +1,44 @@
 import streamlit as st
 from prompt import get_text
-st.title("Hello!")
-models = ["orca-mini-3b-gguf2-q4_0.gguf", 
-          "en annen modell",
-          "en tredje modell",
-          ]
+st.title("Bli kjent med Delfin.AI 🐬")
+st.markdown("""
+Delfin.ai er en innovativ platform som anvender kunstig intelligens (KI)-modeller på sensitive data direkte på Sikkerhetsklarerte Norske servere. Målet er å tilby en sikker, effektiv og skalerbar plattform som møter behovene til bedrifter og organisasjoner som håndterer skjermingsverdig informasjon.
+""")
 
-chosen_model = st.radio("choose model", models)
-prompt = st.text_input("Prompt")
-response = get_text(prompt, chosen_model)
-st.write(response)
+modelvalg = [
+    "Llama 8B",
+    "Llama 4B",
+    "Ocra Mini 3B",
+]
+models = st.radio("Velg modell", modelvalg)
+
+technical_model = {
+    "Llama 8B": "Meta-Llama-3-8B-Instruct-Q6_K.gguf",
+    "Llama 4B": "Meta-Llama-3-8B-Instruct.Q4_0.gguf",
+    "Ocra Mini 3B": "orca-mini-3b-gguf2-q4_0.gguf",
+}
+model = technical_model[models]
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+# React to user input
+if prompt := st.chat_input("What is up?"):
+    # Display user message in chat message container
+    st.chat_message("user").markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    ki_response = get_text(prompt, model)
+    response = f"Delfin: {ki_response}"
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        st.markdown(ki_response)
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": ki_response})
+    
+
--- prompt.py
+++ prompt.py
@@ -1,5 +1,5 @@
 from gpt4all import GPT4All
-def get_text(prompt, chosen_model):
+def get_text(prompt, chosen_model, max_tokens=1024):
     model = GPT4All(chosen_model) 
     with model.chat_session():
-        return(model.generate(prompt, max_tokens=1024))
+        return(model.generate(prompt, max_tokens=max_tokens))
